554dcbf0855d7fedac76d121e3e25d95a0abe0b6
David Mead <dem@vamead.net>
2018-08-31 15:55:28 -0400

    tried to do part 2 of 8puzzle, failed when working with a 2X2 grid, need to work on converting the index of 0 into an (x, y) coordinate system

--- puzzle8.py
+++ puzzle8.py
@@ -1,69 +1,104 @@
 import sys
+from collections import deque
 import math
 
 def main():
-    start, size = getStartState()
-    display(start, size)
-    start = moveLeft(start, size)
-    start = moveRight(start, size)
-    start = moveUp(start, size)
-    start = moveDown(start, size)
+    global size
+    size = 2
+    getAllWinnable()
+    # start, size = getStartState()
+    # display(start)
+    # start = moveLeft(start)
+    # start = moveRight(start)
+    # start = moveUp(start)
+    # start = moveDown(start)
+
+
+def getAllWinnable():
+    start = "0123"
+    fringe = deque()
+    fringe.append(start)
+    visited = {start, }
+
+    while not len(fringe) is 0:
+        v = fringe.pop()
+        for child in getChildren(v):
+            if not child in visited:
+                fringe.append(child)
+                visited.add(child)
+    print(len(visited))
 
 
 
-
-
+def getChildren(state):
+    children = {moveLeft(state), moveDown(state), moveRight(state), moveUp(state)}
+    return children
 
 
 #MARK: MOVE FUNCS
 
-def moveLeft(state, size):
+def moveLeft(state):
     i = state.index("0")
     if i % size is not 0:
         newState = state[:i-1] + state[i] + state[i-1] + state[i+1:]
-        display(newState, size)
+        display(newState)
         return(newState)
     else:
-        print("can't move left here")
+        #print("can't move left here")
         return(state)
 
-def moveRight(state, size):
+def moveRight(state):
+    #print(state)
     i = state.index("0")
-    if i-size % size is not 0:
+    #print(i)
+    #print((i-size+1) % size)
+    if (i-size+1) % size is not 0:
         newState = state[:i] + state[i+1] + state[i] + state[i+2:]
-        display(newState, size)
+        display(newState)
         return(newState)
     else:
-        print("can't move right here")
+        #print("can't move right here")
         return(state)
 
-def moveUp(state, size):
+def moveUp(state):
     i = state.index("0")
     if i > size-1:
-        newState = state[:i-size] + state[i] + state[i-2:i] + state[i-size] + state[i+1:]
-        display(newState, size)
+        newState = state[:i-size] + state[i] + state[i-size-1:i] + state[i-size] + state[i+1:]
+        display(newState)
         return(newState)
     else:
-        print("can't move up here")
+        #print("can't move up here")
         return(state)
 
-def moveDown(state, size):
+def moveDown(state):
     i = state.index("0")
     if i < size*(size-1):
         newState = state[:i] + state[i+size] + state[i+1:i+size] + state[i] + state[i+size+1:]
-        display(newState, size)
+        display(newState)
         return(newState)
     else:
-        print("can't move down here")
+        #print("can't move down here")
         return(state)
+
+
+#MARK: REQUIRED FUNCS
+def get_children(state):
+    print("hello world")
+
+def goal_test(state):
+    print("hello world")
+
+
+def print_puzzle(state):
+    display(state)
 
 
 #MARK: MISC FUNCS
 
-def display(state, size):
+def display(state):
     for x in range(0, size):
         print(" ".join(state[x*size:(x+1)*size]))
-    print()
+    print("")
 
 def getStartState():
     print("start state: " + sys.argv[1])
