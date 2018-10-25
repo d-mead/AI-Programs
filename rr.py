d88e3c7ebf81411f73a46267819c23d27185c847
David Mead <dem@vamead.net>
2018-10-25 11:54:00 -0400

    about to submit rr part 1

--- rr.py
+++ rr.py
@@ -1,7 +1,9 @@
 from math import pi , acos , sin , cos
 from collections import deque
 import time
+import sys
 from heapq import heappush, heappop
+import pickle
 # import pickle
 
 
@@ -9,42 +11,63 @@ def main():
     global edges, nodes, names
     edges, nodes = make_edges_dict("rrNodes.txt", "rrEdges.txt")
     names = make_names_dict("rrNodeCity.txt")
-    start = "9100373"
-    end = "9100042" #0100007, 4700027
 
-    find_longest()
-    # print(get_children("5500045"))
-    # start = "9100373"
-    # end = "9100042" #0100007, 4700023
+    # with open("edges_236.pkl", "wb") as outfile:
+    #     pickle.dump(edges, outfile)
+    # with open("nodes_236.pkl", "wb") as outfile:
+    #     pickle.dump(nodes, outfile)
+    # with open("names_236.pkl", "wb") as outfile:
+    #     pickle.dump(names, outfile)
     #
-    # begin = time.perf_counter()
-    # dij = dijkstra(start, end)
-    # stop = time.perf_counter()
-    # print("Dijkstra: \t%s, %s seconds" % (dij, round(stop - begin, 5)))
-    #
-    #
-    # begin = time.perf_counter()
-    # a = a_star(start, end)
-    # stop = time.perf_counter()
-    # print("A-Star: \t%s, %s seconds" % (a, round(stop - begin, 5)))
+    # with open("edges_236.pkl", "rb") as infile:
+    #     edges = pickle.load(infile)
+    # with open("nodes_236.pkl", "rb") as infile:
+    #     nodes = pickle.load(infile)
+    # with open("names_236.pkl", "rb") as infile:
+    #     names = pickle.load(infile)
+
+    start = sys.argv[1]
+    end = sys.argv[2]
+
+    start_id = names[start]
+    end_id = names[end]
+    
+    begin = time.perf_counter()
+    dij = dijkstra(start_id, end_id)
+    stop = time.perf_counter()
+    print("Dijkstra: \t%s \t%s seconds" % (dij, round(stop - begin, 5)))
+
+
+    begin = time.perf_counter()
+    a = a_star(start_id, end_id)
+    stop = time.perf_counter()
+    print("A-Star: \t%s \t%s seconds" % (a, round(stop - begin, 5)))
 
 
 def a_star(start, end):
     fringe = [(0, 0, start, nodes[start][0],  0), ]
     visited = set()
 
+    end_y = nodes[end][0][0]
+    end_x = nodes[end][0][1]
+
     while len(fringe) is not 0:
         s = heappop(fringe)
 
         if goal_test(s[2], end):  # if the state is won
-            return s[3]  # return the moves
+            return s[0]  # return the moves
+
+        if s[2] in visited:
+            continue
+
+        visited.add(s[2])
 
         children = get_children(s[2])
         for child in children:
             if child[1] not in visited:
-                circle = calcd(child[2][0], child[2][1], s[2][0], s[2][1])
-                heappush(fringe, (circle+s[1], child[0], child[1], s[1]+child[0]))
-                visited.add(child[1])
+                circle = calcd(child[2][0], child[2][1], end_y, end_x)
+                heappush(fringe, (circle+s[4]+child[0], child[0], child[1], child[2], s[4]+child[0]))
+
     if len(fringe) is 0:
         return -1
 
@@ -52,22 +75,25 @@ def a_star(start, end):
 def dijkstra(start, end):
     fringe = [(0, start, 0), ]
     visited = set()
-    visited.add(start)
 
     while len(fringe) is not 0:
         s = heappop(fringe)
+
         if goal_test(s[1], end):  # if the state is won
             return s[2]  # return the moves
+
+        if s[1] in visited:
+            continue
+
+        visited.add(s[1])
+
         children = get_children(s[1])
         for child in children:
             if child[1] not in visited:
-                heappush(fringe, (child[0], child[1], s[0]+child[0]))
-                visited.add(child[1])
+                heappush(fringe, (s[2]+child[0], child[1], s[2]+child[0]))
 
     if len(fringe) is 0:
         return -1
-
-# def run(start, end):
 
 
 def get_children(location):
@@ -96,6 +122,10 @@ def make_edges_dict(nodes_name, edges_name):
             edges.get(line[0]).append((dist, line[1], nodes[line[1]][0]))
         else:
             edges[line[0]] = [(dist, line[1], nodes[line[1]][0])]
+        if line[1] in edges.keys():
+            edges.get(line[1]).append((dist, line[0], nodes[line[0]][0]))
+        else:
+            edges[line[1]] = [(dist, line[0], nodes[line[0]][0])]
     for node in nodes.keys():
         if node not in edges.keys():
             edges[node] = []
