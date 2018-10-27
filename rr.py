616d83c16d31be216e00bf294db7d34ec65f561f
David Mead <dem@vamead.net>
2018-10-26 20:22:44 -0400

    working on displaying it

--- rr.py
+++ rr.py
@@ -3,8 +3,12 @@ from collections import deque
 import time
 import sys
 from heapq import heappush, heappop
+from tkinter import *
 import pickle
 # import pickle
+from PIL import ImageTk
+import utm
+
 
 
 def main():
@@ -26,25 +30,129 @@ def main():
     # with open("names_236.pkl", "rb") as infile:
     #     names = pickle.load(infile)
 
-    start = sys.argv[1]
-    end = sys.argv[2]
+    # start = "Albuquerque"  # sys.argv[1]
+    # end = "Dallas"  # sys.argv[2]
+    #
+    # start_id = names[start]
+    # end_id = names[end]
+    #
+    # begin = time.perf_counter()
+    # dij = dijkstra(start_id, end_id)
+    # stop = time.perf_counter()
+    # print("Dijkstra: \t%s \t%s seconds" % (dij, round(stop - begin, 5)))
+    #
+    #
+    # begin = time.perf_counter()
+    # a = a_star(start_id, end_id)
+    # stop = time.perf_counter()
+    # print("A-Star: \t%s \t%s seconds" % (a, round(stop - begin, 5)))
+    #
+    # draw()
 
+    utm_nodes = dict()
+
+    for node, loc in nodes.items():
+        utm_nodes[node] = utm.from_latlon(loc[0][0], loc[0][1])[0:2]
+
+    global minx, maxx, miny, maxy, scalew, scaleh, height, width, shift
+    minx = list(utm_nodes.values())[0][1]
+    maxx = list(utm_nodes.values())[0][1]
+    miny = list(utm_nodes.values())[0][0]
+    maxy = list(utm_nodes.values())[0][0]
+    scalew = 10.0
+    scaleh = 10.0
+    shift = 0.0
+
+    for node, loc in utm_nodes.items():
+        if loc[0]<miny:
+            miny = loc[0]
+        if loc[0]>maxy:
+            maxy = loc[0]
+        if loc[1]<minx:
+            minx = loc[1]
+        if loc[1]>maxx:
+            maxx = loc[1]
+
+    print(miny, maxy, minx, maxx)
+
+    height = abs(maxx-minx)
+    width = abs(maxy-miny)
+
+    print(height, width)
+
+    scalew = 921/width# 12
+    scaleh = 814/height# 10
+
+    start = "Chicago"
     start_id = names[start]
-    end_id = names[end]
-    
-    begin = time.perf_counter()
-    dij = dijkstra(start_id, end_id)
-    stop = time.perf_counter()
-    print("Dijkstra: \t%s \t%s seconds" % (dij, round(stop - begin, 5)))
+
+    draw(full_send(start_id))
 
 
-    begin = time.perf_counter()
-    a = a_star(start_id, end_id)
-    stop = time.perf_counter()
-    print("A-Star: \t%s \t%s seconds" % (a, round(stop - begin, 5)))
+def draw(lines):
+    global minx, maxx, miny, maxy, scalew, scaleh, height, width
+    master = Tk()
+
+    w = Canvas(master, width=int(width)*scalew, height=int(height)*scaleh)
+    w.pack()
+
+    image = ImageTk.PhotoImage(file="rrImage.png")
+    # backgroundLabel = master.Label(parent, image=image)
+    w.create_image(0, 0, image=image, anchor=NW)
+    print(image.height())
+
+    for line in lines[:20]:
+        print(line)
+        w.create_line(line)
+
+    mainloop()
+
+
+def full_send(start):
+    global minx, maxx, miny, maxy, scaleh, scalew
+
+    lines = []
+
+    fringe = [(0, 0, start, nodes[start][0], 0), ]
+    visited = set()
+
+    while len(fringe) is not 0:
+        s = heappop(fringe)
+
+        if s[2] in visited:
+            continue
+
+        visited.add(s[2])
+
+        children = get_children(s[2])
+        for child in children:
+            if child[1] not in visited:
+                heappush(fringe, (0, child[0], child[1], child[2], s[4] + child[0]))
+
+                lines.append(((abs(float(s[3][1]))+minx)*scaleh, abs((float(s[3][0]))+miny)*scalew, (abs(float(child[2][1]))+minx)*scaleh, abs((float(child[2][0]))+miny)*scalew))
+                #w.create_line((abs(float(s[3][1])) - 70) * 20, (abs(float(s[3][0])) - 30) * 20, (abs(float(child[2][1])) - 70) * 20, (abs(float(child[2][0])) - 30) * 20)
+
+    if len(fringe) is 0:
+        return lines
+
+def draw_0():
+    master = Tk()
+
+    w = Canvas(master, width=200, height=100)
+    w.pack()
+
+    w.create_line(0, 0, 200, 100)
+    w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+
+    mainloop()
 
 
 def a_star(start, end):
+    master = Tk()
+
+    w = Canvas(master, width=800, height=400) #30 to 50 y, 70 to 100 x
+    w.pack()
+
     fringe = [(0, 0, start, nodes[start][0],  0), ]
     visited = set()
 
@@ -65,8 +173,15 @@ def a_star(start, end):
         children = get_children(s[2])
         for child in children:
             if child[1] not in visited:
-                circle = calcd(child[2][0], child[2][1], end_y, end_x)
+                try:
+                    circle = calcd(child[2][0], child[2][1], end_y, end_x)
+                except ValueError:
+                    circle = 0
                 heappush(fringe, (circle+s[4]+child[0], child[0], child[1], child[2], s[4]+child[0]))
+                #print(abs(float(s[3][1]))-70, abs(float(s[3][0]))-30, abs(float(child[2][1]))-70, abs(float(child[2][0]))-30)
+                w.create_line((abs(float(s[3][1]))-70)*20, (abs(float(s[3][0]))-30)*20, (abs(float(child[2][1]))-70)*20, (abs(float(child[2][0]))-30)*20)
+
+    mainloop()
 
     if len(fringe) is 0:
         return -1
@@ -101,9 +216,13 @@ def get_children(location):
 
 
 def find_longest():
+    do = True
     for start, ids in names.items():
         for end, ide in names.items():
-            print("%s, %s: %s" % (start, end, a_star(ids, ide)))
+            if start == "Brooklyn":
+                do = False
+            if do:
+                print("%s, %s: %s" % (start, end, a_star(ids, ide)))
 
 
 def make_edges_dict(nodes_name, edges_name):
