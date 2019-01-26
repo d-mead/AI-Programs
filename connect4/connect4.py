a5b6a8e436d888840290efb00c7a251ba2df615f
David Mead <dem@vamead.net>
2019-01-25 23:30:45 -0500

    random game with animations working

--- connect4/connect4.py
+++ connect4/connect4.py
@@ -1,19 +1,45 @@
 BLANK = "??????????.......??.......??.......??.......??.......??.......???????????"
-from collections import deque
-# from graphics import *
-# from PIL import *
 
+import time
+import tkinter as tk
+from PIL import ImageTk
+import random
 
 def main():
-    display(BLANK)
+    global master
+    blank_window()
+    time.sleep(1)
+    # display(BLANK)
+
+    random_game()
+
+    master.mainloop()
+
+
+def random_game():
     board = BLANK
-    board = drop(board, 0, '@')
-    board = drop(board, 1, 'o')
-    board = drop(board, 2, '@')
-    board = drop(board, 3, '@')
-    display(board)
-    print(get_valid_cols(board))
-    print(check_win(board))
+    token = '@'
+    swap = {'@':'o', 'o':'@'}
+
+    while not check_win(board):
+        valid_columns = get_valid_cols(board)
+        if len(valid_columns) == 0:
+            break
+        col = valid_columns[random.randint(0, len(valid_columns)-1)]
+        board = drop(board, col, token)
+        token = swap[token]
+        display(board)
+
+    statement = {"@": "black wins!", 'o': 'red wins!', '.': 'tie game'}
+    print(statement[check_win(board)])
+
+    display_winner(check_win(board))
+
+
+def display_winner(token):
+    statement = {"@": "black wins!", 'o': 'red wins!', '.': 'tie game'}
+    colors = {'@': 'black', 'o': 'red', '.': 'grey'}
+    w.create_text(350, 50, fill=colors[token], font="Helvetica 40 italic bold", text=statement[token])
 
 
 def check_win(board):
@@ -44,9 +70,30 @@ def get_valid_cols(board):
 def drop(board, col, token):
     for row in range(7, 0, -1):
         if board[row*9+col+1] == '.':
-            board = place(board, row*9+col+1, token)
+            animate_drop(board, col+10, row*9+col+1, token)
+            board = place(board, row * 9 + col + 1, token)
             break
     return board
+
+
+def animate_drop(board, start, stop, token):
+    global index_to_cord
+
+    colors = {'@': 'black', 'o': 'red'}
+
+    color = colors[token]
+
+    x = index_to_cord[start][0]
+    start_y = index_to_cord[start][1]
+    end_y = index_to_cord[stop][1]
+
+    for y in range(start_y, end_y, 40):
+        wipe_window()
+        w.create_circle(x, y, 40, outline='', fill=color)
+        display_window(board)
+        time.sleep(.008)
+    wipe_window()
+    display_window(board)
 
 
 def place(board, spot, token):
@@ -58,6 +105,65 @@ def display(board):
     for row in range(1, 7):
         print(" ".join(board[x] for x in range(row*9+1, row*9+8)) + "\t" + str(list(range(row*9+1, row*9+8))))
     print()
+    display_window(board)
+
+
+def display_window(board):
+
+    w.create_image(353, 401, image=photoimage)
+
+    colors = {'@': 'black', 'o': 'red'}
+    spots = [x for x in range(10, 62) if (board[x] != '.' and board[x] != '?')]
+    for spot in spots:
+        token = board[spot]
+        color = colors[token]
+        cord = index_to_cord[spot]
+        w.create_circle(cord[0], cord[1], 40, outline='', fill=color)
+
+    master.update_idletasks()
+    master.update()
+
+
+def wipe_window():
+    w.delete('all')
+
+    for index in index_to_cord.values():
+        w.create_circle(index[0], index[1], 40, outline='', fill='SteelBlue3')
+
+
+def blank_window():
+    global master
+    master = tk.Tk()
+
+    global photoimage
+    photoimage = ImageTk.PhotoImage(file="yellows.png")
+
+    global w
+    w = tk.Canvas(master, width=700, height=700)
+    w.pack()
+
+    w.create_image(353, 401, image=photoimage)
+
+
+    global index_to_cord
+    index_to_cord = dict()
+
+    for row_i, row in zip(range(10, 56, 9), range(0, 7)):
+        for col_i, col in zip(range(row_i, row_i + 7), range(0, 8)):
+            index_to_cord[col_i] = (50 + 100 * col, 150 + 100 * row)
+
+    for index in index_to_cord.values():
+        w.create_circle(index[0], index[1], 40, outline='', fill='SteelBlue3')
+
+    master.update_idletasks()
+    master.update()
+
+
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x - r, y - r, x + r, y + r, **kwargs)
+
+tk.Canvas.create_circle = _create_circle
+
 
 if __name__ == "__main__":
     main()
--- connect4/graphics_learning.py
+++ connect4/graphics_learning.py
@@ -6,33 +6,35 @@ def main():
     w = tk.Canvas(master, width=700, height=700)
     w.pack()
 
+    w.create_rectangle(0, 100, 700, 700, outline='', fill='gold')
+
+    linecolor = 'goldenrod'
     for y in range(200, 701, 100):
-        w.create_line(0, y, 700, y)
+        w.create_line(0, y, 700, y, width=3, fill=linecolor)
 
     for x in range(0, 701, 100):
-        w.create_line(x, 100, x, 700)
-    w.create_line(3, 100, 3, 700)
+        w.create_line(x, 100, x, 700, width=3, fill=linecolor)
+    w.create_line(3, 100, 3, 700, width=6, fill=linecolor)
 
-    w.create_circle(50, 150, 40)
+    # w.create_circle(50, 150, 40)
 
     index_to_cord = dict()
 
     for row_i, row in zip(range(10, 56, 9), range(0, 7)):
             for col_i, col in zip(range(row_i, row_i+7), range(0, 8)):
-                index_to_cord[col_i] = (50+100*col, 150+100*row
+                index_to_cord[col_i] = (50+100*col, 150+100*row)
+
+    for index in index_to_cord.values():
+        w.create_circle(index[0], index[1], 40, outline='', fill='SteelBlue3')
 
     print(index_to_cord)
 
-
-    # w.create_line(100, 100, 200, 100)
-    # w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-
-    # w.create_rectangle(50, 25, 150, 75, fill="blue")
-
     master.mainloop()
+
 
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x - r, y - r, x + r, y + r, **kwargs)
+
 
 tk.Canvas.create_circle = _create_circle
 
